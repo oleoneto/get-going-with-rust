7fdee847579fdf72ae6833ad248cf509cddb98a8
Leo Neto <oleoneto@gmail.com>
2023-06-12 01:44:05 -0300

    feat: add middleware

--- src/api/api.rs
+++ src/api/api.rs
@@ -1,9 +1,29 @@
 pub mod api {
-    use crate::api::books::books;
-    use crate::api::items::items;
+    use crate::api::{books::books, items::items};
+    use std::{future::Future, pin::Pin};
+
+    fn check_content_type_header<'a>(
+        req: tide::Request<()>,
+        next: tide::Next<'a, ()>,
+    ) -> Pin<Box<dyn Future<Output = tide::Result> + Send + 'a>> {
+        Box::pin(async {
+            let content_type = req.header("Content-Type");
+            if req.method() == tide::http::Method::Post
+                && (content_type.is_none() || content_type.unwrap() != "application/json")
+            {
+                // https://doc.rust-lang.org/std/macro.print.html
+                // eprintln!("{:?}", content_type.unwrap());
+                return Ok(tide::Response::new(415));
+            }
+
+            Ok(next.run(req).await)
+        })
+    }
 
     pub async fn listen() -> tide::Result<()> {
         let mut app = tide::new();
+
+        app.with(check_content_type_header);
 
         app.at("/books").nest(books::routes());
 
